1466b6e4146ded46e08d8cb0c7e23fa302a710a5
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-05-15 21:22:57 -0300

    aula 6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,51 +1,33 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel</title>
+@section('title', 'ART Events')
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        
-        <link rel="stylesheet" href="/css/app.css">
-        <script src="/javascript/script.js"></script>
-        
-    </head>
-    <body class="antialiased">
-        <h1>Hello Laravel!</h1>
-        @if(10 > 5)
-                <p>a condição é true</p>
-        @endif
+@section('content')
 
-        <img src="/imgs/capa.jpg" alt="live-banner">
+<h1>Hello Laravel!</h1>
+@if (10 > 5)
+    <p>a condição é true</p>
+@endif
 
-        @if ($nome == "Arthur")
-                <p>Olá, seu nome é {{ $nome }} e você tem {{ $idade }} anos</p>
-        @else
-        <p>O nome não é {{ $nome }}</p>
-        @endif
-        
-        @for ($i = 0; $i < count($arr); $i++)
-        <p>{{ $arr[$i] }}</p>
-        @if ($i == 2)
-            <p> o indice é 2 </p>
-        @endif
-        @endfor
+<img src="/imgs/capa.jpg" alt="live-banner">
 
-        @foreach($nomes as $names)
-        <br>
-        <p>{{ $loop->index }}</p>
-            <p>{{ $names }}</p>
-        @endforeach
-        
-        <br>
-        @php
-            $name = 'Arthur';
-            echo $name;
-        @endphp
+@if ($nome == 'Arthur')
+    <p>Olá, seu nome é {{ $nome }} e você tem {{ $idade }} anos</p>
+@else
+    <p>O nome não é {{ $nome }}</p>
+@endif
 
-    </body>
-</html>
+@for ($i = 0; $i < count($arr); $i++)
+    <p>{{ $arr[$i] }}</p>
+    @if ($i == 2)
+        <p> o indice é 2 </p>
+    @endif
+@endfor
+
+@foreach ($nomes as $names)
+    <br>
+    <p>{{ $loop->index }}</p>
+    <p>{{ $names }}</p>
+@endforeach
+
+@endsection

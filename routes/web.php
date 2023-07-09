4fc8d84db5ca3762abb1dbd86c6e28d4c60e0ae1
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-07-09 20:28:19 -0300

    modulo 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     echo 'Olá Mundo';
 });
+
+Route::get('/series', 
+    [SeriesController::class, 'index']
+);
